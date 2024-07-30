9468a41d0806eec115b666d1c4bf635c19692df5
ararsa <ararsaderese6@gmail.com>
2024-07-30 12:01:46 +0300

    :white_check_mark: validate Task 1

--- Task1/gradeCalculator.go
+++ Task1/gradeCalculator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func calculateAverage(grades map[string]int) float64 {
@@ -12,30 +13,57 @@ func calculateAverage(grades map[string]int) float64 {
 	return sum / float64(len(grades))
 }
 
+func validatename(name string) bool {
+	for _, char := range name {
+		if (char < 'a' || char > 'z') && (char < 'A' || char > 'Z') {
+			return false
+		}
+	}
+	return len(name) > 0
+}
+
+
 func main() {
 
 	var name string
-	var subject int
+	var subject string
+entername:
 	fmt.Print("Enter your name: ")
-	_, err := fmt.Scan(&name)
+	_, err := fmt.Scanln(&name)
+	if !validatename(name) {
+		fmt.Println("Name should only contain letters")
+		goto entername
+	}
 	if err != nil {
-		fmt.Println("Error in reading input")
+		fmt.Println("error reading data")
 		return
 	}
+entersubject:
 	fmt.Print("Enter How many subjects you take: ")
 	_, err = fmt.Scan(&subject)
 	if err != nil {
 		fmt.Println("Error in reading input")
-		return
+		goto entersubject
+	}
+	
+	 num, err := strconv.Atoi(subject)
+	if err != nil {
+		fmt.Println("Please enter a number not a letter")
+		goto entersubject
 	}
 
 	subjectswithgrades := make(map[string]int)
-
-	for i := 0; i < subject; i++ {
+	for i := 0; i < num; i++ {
 		var subjectname string
-		var grade int
+		var grade string
+	entersubjectname:
 		fmt.Print("Enter subject name ", i+1, ": ")
 		_, err = fmt.Scan(&subjectname)
+		if !validatename(subjectname) {
+			fmt.Println("Subject name should only contain letters")
+			goto entersubjectname
+		}
+
 		if err != nil {
 			fmt.Println("Error in reading input")
 			return
@@ -45,13 +73,19 @@ func main() {
 		_, err = fmt.Scan(&grade)
 		if err != nil {
 			fmt.Println("Error in reading input")
-			return
+			goto entersubject
 		}
-		if grade < 0 || grade > 100 {
+		
+		g, err := strconv.Atoi(grade)
+		if err != nil {
+			fmt.Println("Please enter a number not a letter")
+			goto reEnter
+		}
+		if g < 0 || g > 100 {
 			fmt.Println("Grade should be between 0 and 100")
 			goto reEnter
 		}
-		subjectswithgrades[subjectname] = grade
+		subjectswithgrades[subjectname] = g
 	}
 
 	fmt.Println("Your average is: ", calculateAverage(subjectswithgrades))
